d3b80bb10b86a9d770481cfe10d335b7f299655a
Mark C Allen <mark@markcallen.com>
2020-03-27 16:06:25 -0700

    Added script to bootstrap dotfiles on osx and linux

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles
+
+## Install
+
+curl https://raw.githubusercontent.com/markcallen/dotfiles/master/terminal_install.sh | bash -s 
---
+++ terminal-setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# OS
+osName="$(uname -s)"
+case "${osName}" in
+  Linux*)  os=Linux;;
+  Darwin*) os=Mac;;
+  *)       exit 1;;
+esac
+
+echo "Setup terminal for $os"
+
+# Go Home
+pushd $HOME
+
+# Create .ssh key
+if [ ! -d ~/.ssh ]; then
+  mkdir ~/.ssh
+  chmod 700 .ssh
+fi
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -t rsa -b 4096 -C $(whoami)@$(hostname) -f ~/.ssh/id_rsa -q -N ""
+fi
+
+# Install
+
+echo "Installing necessary packages for development"
+if [ $os == "Linux" ]; then
+  sudo apt install -y tmux vim git powerline python3 python3-pip
+elif [ $os == "Darwin" ]; then
+  if [ ! -x brew ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    xcode-select --install
+  fi
+  brew install python3 tmux 
+fi
+
+pip3 install powerline-status
+pip3 install --user powerline-gitstatus
+
+
+# Get dotfiles
+if [ ! -d ~/.dotfiles ]; then
+  git clone https://github.com/markcallen/dotfiles ~/.dotfiles
+else
+  pushd .dotfiles && git pull && popd
+fi
+
+pushd .dotfiles && ./install && popd
+
+# Go back to where we started from
+popd
+
+exit 0;
