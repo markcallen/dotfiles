91c44e4310fe71dd44e559ad8cb8a66d4593a051
Mark C Allen <mark@markcallen.com>
2020-07-27 17:27:11 +0000

    Added vim repo for ubuntu

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -27,7 +27,9 @@ fi
 
 echo "Installing necessary packages for development"
 if [ $os == "Linux" ]; then
-  sudo apt install -y tmux vim git powerline python3 python3-pip jq
+  sudo sudo add-apt-repository ppa:jonathonf/vim
+  sudo apt-get update
+  sudo apt install -y tmux vim git powerline python3 python3-pip jq vim
 elif [ $os == "Darwin" ]; then
   if [ ! -x brew ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
