2d4bec100a29b3c526b385c26d52bf5290e241cc
Mark C Allen <mark@markcallen.com>
2022-08-09 11:33:21 -0700

    Fixed missing sudo

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -51,9 +51,9 @@ pip3 install --user powerline-gitstatus
 if [ $os == "Linux" ]; then
    wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
    wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-   mv PowerlineSymbols.otf /usr/share/fonts/
+   sudo mv PowerlineSymbols.otf /usr/share/fonts/
    fc-cache -vf /usr/share/fonts/
-   mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+   sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 elif [ $os == "Darwin" ]; then
   git clone https://github.com/powerline/fonts.git --depth=1
   pushd fonts && ./install.sh && popd
