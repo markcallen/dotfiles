3e6e5c2a1c749ca6aab11a8292609190510ee41e
Mark C Allen <mark@markcallen.com>
2022-12-20 17:14:24 -0800

    Added nvm to current shell so next commands will work

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -42,6 +42,9 @@ fi
 
 echo "Install nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 16
 nvm alias default 16
 
