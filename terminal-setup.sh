14a24aedc654488922655709f8f75a7be306407d
Mark C Allen <mark@markcallen.com>
2025-08-09 06:45:53 +0000

    Added vim back in to get the correct version on linux

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -38,6 +38,7 @@ if [ $os == "Linux" ]; then
   if [ -f /etc/needrestart/needrestart.conf ]; then
     sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
   fi
+  sudo add-apt-repository -y ppa:jonathonf/vim
   sudo apt-get update
   sudo apt-get install -y build-essential
   sudo apt install -y git bash-completion powerline silversearcher-ag fzf dnsutils iputils-ping
