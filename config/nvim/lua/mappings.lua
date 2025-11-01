76293e9e33a9aa7461fd669c19d9f391e2c1fa26
Mark C Allen <mark@markcallen.com>
2025-11-01 23:31:54 +0000

    Added config to vim-tmux-navigation

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -3,8 +3,22 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+
+--- This fixes an issue with the vim-tmux-navigator + nvchad in which the base nvchad
+--- mapping were conflicting with vim-tmux-navigator ones.
+unmap("n", "<c-h>")
+unmap("n", "<c-j>")
+unmap("n", "<c-k>")
+unmap("n", "<c-l>")
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>:TmuxNavigatePrevious<cr>")
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -5,6 +5,18 @@ return {
     opts = require "configs.conform",
   },
 
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+    lazy = false,
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
